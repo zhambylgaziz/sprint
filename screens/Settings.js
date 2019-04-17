c3fc2ea037cc6b7dda75fcfcaf3466cfc45acb03
kenesovb <39025730+kenesovb@users.noreply.github.com>
2019-04-17 18:00:48 +0600

    Update Settings.js

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,29 +1,54 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
 
 export default class Settings extends React.Component {
-  render() {
-    return (
-      <View style = { styles.container } > 
-        <Text style = { styles.center } > 
-          This is an settings page  
-        </Text>
-      </View>
-    );
+    render() {
+      return (
+        <View style={styles.container}>
+    {
+     list.map((item, i) => (
+             <ListItem button onPress ={()=> this.props.navigation.navigate(item.page)}
+             key={i}
+             title={item.name}
+        />
+        ))
+    }
+    </View>
+   )
   }
-}
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 30,
-    paddingVertical: 30
-  },
-  header: {
-    fontSize: 12,
-    fontFamily: 'Cochin'
-  },
-  center: {
-    fontSize: 12,
-    textAlign: 'center' 
-  }
+ }
+  const list = [
+      {
+        name: 'Войти',
+        page: 'Login'
+      },
+      {
+        name: 'Зарегистрироваться',
+        page: 'Register'
+      }
+    ,
+    {
+      name: 'О нас',
+      page: 'About'
+    },
+    {
+      name: 'Отзывы',
+      page: 'Feedback'
+    }
+  ]
+  const styles = StyleSheet.create({
+    container: {
+      paddingHorizontal: 30,
+      paddingVertical: 30
+    },
+    header: {
+      fontSize: 12,
+      fontFamily: 'Cochin'
+    },
+    center: {
+      fontSize: 12,
+      textAlign: 'center'
+    }
 
-})
+  })
