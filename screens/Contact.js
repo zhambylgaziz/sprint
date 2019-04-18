585af517c43bb7b4ed7c1c69da8496c3277bc9a7
Zhambyl Gaziz <zhambylgaziz@gmail.com>
2019-04-18 16:41:16 +0600

    contacts

---
+++ screens/Contact.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Catalog extends React.Component {
+  render() {
+    return (
+      <View style = { styles.container } > 
+        <Text> 
+          Sprint dev
+        </Text>
+        <Text style = { styles.left }>
+          Kelsingazin Yerassyl
+          8(777)2579952
+        </Text>
+        <Text style = { styles.left }>
+          Kenges Bainur
+          8(702)9933227
+        </Text>
+        <Text style = { styles.left }>
+          Gaziz Zhambyl
+          8(700)0881551
+        </Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 30
+  },
+  header: {
+    fontSize: 12,
+    fontFamily: 'Cochin'
+  },
+  center: {
+    fontSize: 12,
+    textAlign: 'center' 
+  },
+  left {
+    fontSize: 12,
+    textAlign: 'left'
+    padding: '5, string'
+  }
+
+})
